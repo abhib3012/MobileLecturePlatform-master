33a8d9c0a02c581764368d0d50fe3c94dfa5bed6
Abhishek Bhattacharya <abhishek@Abhisheks-MacBook-Air.local>
2016-06-22 10:28:58 +0530

    Container Symbol Solved

--- app/src/main/java/com/example/abhishek/mobilelectureplatform/lectureActivity.java
+++ app/src/main/java/com/example/abhishek/mobilelectureplatform/lectureActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class lectureActivity extends AppCompatActivity {
 
+    public static final String FRAGMENT_PDF_RENDERER_BASIC = "pdf_renderer_basic";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PdfRendererBasicFragment(),
+                            FRAGMENT_PDF_RENDERER_BASIC)
+                    .commit();
     }
 }
